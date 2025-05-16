38f5a93394221d0bb5fa277efb5280e6e10175c2
Ashish Kushwaha <ashishkushwahaaaa@gmail.com>
2025-05-16 15:24:14 +0530

    Revert "added bottom navigation bar"

--- app/src/main/java/com/ashish/campusconnect/screen/Screen.kt
+++ app/src/main/java/com/ashish/campusconnect/screen/Screen.kt
@@ -1,11 +1,5 @@
 package com.ashish.campusconnect.screen
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.ui.graphics.vector.ImageVector
-
 sealed class Screen (val route: String){
     object SplashScreen: Screen("splash_screen")
     object SignUpScreen: Screen("signup_screen")
@@ -14,8 +8,4 @@ sealed class Screen (val route: String){
     object PostScreen: Screen("post_screen")
     object PostDetailsScreen: Screen("post_details_screen")
     object CampusIdVerificationScreen: Screen("campus_id_verification_screen")
-    object Profile : Screen("profile_screen")
-    object Event : Screen("event_screen")
-    object courses : Screen("courses_screen")
 }
-
