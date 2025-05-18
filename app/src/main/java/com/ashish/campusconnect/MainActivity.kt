f30bad98e39753f4b6289ba6c4110496c65c9ec6
Ashishkushwahaaaa <ashishkushwahaaaa@gmail.com>
2025-05-18 15:06:28 +0530

    Renamed HomeScreen to Update and homeViewModel as updateViewModel

--- app/src/main/java/com/ashish/campusconnect/MainActivity.kt
+++ app/src/main/java/com/ashish/campusconnect/MainActivity.kt
@@ -3,24 +3,14 @@ package com.ashish.campusconnect
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
-import com.ashish.campusconnect.data.SessionManager
 import com.ashish.campusconnect.screen.NavigationGraph
-import com.ashish.campusconnect.screen.Screen
 import com.ashish.campusconnect.ui.theme.CampusConnectTheme
-import com.ashish.campusconnect.viewmodel.HomeViewModel
-import com.google.firebase.auth.FirebaseAuth
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/ashish/campusconnect/screen/Screen.kt
+++ app/src/main/java/com/ashish/campusconnect/screen/Screen.kt
@@ -4,7 +4,7 @@ sealed class Screen (val route: String){
     object SplashScreen: Screen("splash_screen")
     object SignUpScreen: Screen("signup_screen")
     object LoginScreen: Screen("login_screen")
-    object HomeScreen: Screen("home_screen")
+    object UpdateScreen: Screen("update_screen")
     object PostScreen: Screen("post_screen")
     object PostDetailsScreen: Screen("post_details_screen")
     object CampusIdVerificationScreen: Screen("campus_id_verification_screen")
